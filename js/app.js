38389ac34305f40c043e6cebd2ce5e8de8394db2
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-06-13 20:40:46 +0100

    basics

--- js/app.js
+++ js/app.js
@@ -4,11 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const deleteAllButton = document.querySelector('#delete-all');
     deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
+    const outfitSeletion = document.querySelector('#outfit-choice');
+    outfitSeletion.addEventListener('submit', handleOutfitChoice);
+
+    const deleteOutfit = document.querySelector('#delete-outfit');
+    deleteOutfit.addEventListener('click', handleDeleteOutfitClick)
 })
 
 const handleDeleteAllClick = function (event) {
     const assembleYourTeam = document.querySelector('#assemble-your-team');
     assembleYourTeam.innerHTML = '';
+    
+}
+
+const handleDeleteOutfitClick = function (event) {
+    const fighterOutfit = document.querySelector('#fighter-outfit');
+    fighterOutfit.innerHTML = '';
 }
 
 const handleNewItemFormSubmit = function (event) {
@@ -21,6 +33,18 @@ const handleNewItemFormSubmit = function (event) {
 
     event.target.reset();
 }
+
+const handleOutfitChoice = function (event) {
+    event.preventDefault();
+
+    const fighterOutfitItem = createFighterOutfitChoiceItem(event.target);
+    const fighterOutfitChoice = document.querySelector('#fighter-outfit');
+    fighterOutfitChoice.appendChild(fighterOutfitItem)
+
+    event.target.reset();
+}
+
+
 
 const createAssembleYourTeamItem = function (form) {
     const assembleYourteamItem = document.createElement('li');
@@ -39,4 +63,15 @@ const createAssembleYourTeamItem = function (form) {
     assembleYourteamItem.appendChild(fightingStyle);
 
     return assembleYourteamItem;
+}
+
+const createFighterOutfitChoiceItem = function (form) {
+    const fighterOutfitItem = document.createElement('li');
+    fighterOutfitItem.classList.add('fighter-outfit')
+
+    const choice = document.createElement('h2');
+    choice.textContent = form.outfit.value;
+    fighterOutfitItem.appendChild(choice)
+
+    return fighterOutfitItem
 }
